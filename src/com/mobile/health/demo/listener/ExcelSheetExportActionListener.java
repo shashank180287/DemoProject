6e53467d3075b45eb35ec381ed9173b351f8d29a
https://github.com/settings/profile <shashank.agrawal@onmobile.com>
2014-03-24 20:26:07 +1100

    Update ExcelSheetExportActionListener.java

--- src/com/mobile/health/demo/listener/ExcelSheetExportActionListener.java
+++ src/com/mobile/health/demo/listener/ExcelSheetExportActionListener.java
@@ -1,11 +1,23 @@
 package com.mobile.health.demo.listener;
 
+import static javax.swing.JOptionPane.showMessageDialog;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.util.List;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.mobile.health.demo.entity.PersonDetails;
+import com.mobile.health.demo.manager.DBManger;
 
 public class ExcelSheetExportActionListener implements ActionListener {
 
@@ -35,8 +47,41 @@ public class ExcelSheetExportActionListener implements ActionListener {
         if (saveChoice == JFileChooser.APPROVE_OPTION) {
             //Put save file code in here
             File saveFile = fileDialog.getSelectedFile();
-            // tracker.append("\nThe file selected is " + saveFile.getName());
-            // tracker.append("\nThe file's path is " + saveFile.getPath());
+            FileTypeFilter fileFilter = (FileTypeFilter) fileDialog.getFileFilter();
+            String extension = fileFilter.getExtension();
+            String fileName = saveFile.getName();
+            if (!fileName.endsWith(extension))
+                saveFile = new File(saveFile.getAbsolutePath() + extension);
+            writeExcelSheet(saveFile);
+        }
+    }
+
+    private void writeExcelSheet(File saveFile) {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Persons Data");
+        List<PersonDetails> personDetails = DBManger.getPersonDetails();
+        int rownum = 0;
+        for (PersonDetails personDetail : personDetails) {
+            Row row = sheet.createRow(rownum++);
+            Object[] objArr = personDetail.toObjectArray();
+            int cellnum = 0;
+            for (Object obj : objArr) {
+                Cell cell = row.createCell(cellnum++);
+                if (obj instanceof String)
+                    cell.setCellValue((String) obj);
+                else if (obj instanceof Integer)
+                    cell.setCellValue((Integer) obj);
+            }
+        }
+        try {
+            //Write the workbook in file system
+            FileOutputStream out = new FileOutputStream(saveFile);
+            workbook.write(out);
+            out.close();
+            showMessageDialog(frame, "File written successfully.");
+        }
+        catch (Exception e) {
+            showMessageDialog(frame, "Some error occured. Please try after some time.");
         }
     }
 }
