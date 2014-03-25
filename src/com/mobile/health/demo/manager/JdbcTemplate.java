27c47c588c121495cf8bc84645c2622155851abb
https://github.com/settings/profile <shashank.agrawal@onmobile.com>
2014-03-25 21:03:54 +1100

    Update JdbcTemplate.java

--- src/com/mobile/health/demo/manager/JdbcTemplate.java
+++ src/com/mobile/health/demo/manager/JdbcTemplate.java
@@ -7,105 +7,66 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JdbcTemplate {
+	private static JdbcTemplate jdbcTemplate;
+	private Connection connection;
 
-	 public Connection getConnection() {
-		  Connection con = null;
-		  try {
-		   //load the HSQLDB Database Driver. 
-		   //This gets loaded from the hsqldb-xxx.jar
-		   Class.forName("org.hsqldb.jdbcDriver");
-		  } catch (ClassNotFoundException cnfe) {
-		   System.out.println("ERROR: failed to load HSQLDB JDBC driver.");
-		   cnfe.printStackTrace();
-		  }
-		   
-		  try {
-		   //connect to the database. 
-		   con = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/mytestdb", "sa", ""); 
-		  }
-		  catch (SQLException e) {
-		   System.out.println("ERROR: failed to connect to the databse");
-		   e.printStackTrace();
-		  }
-		   
-		  return con;
-		 }
-		  
-		  
-		 public void executeInsertQuery(Connection con) {
-		  PreparedStatement ps = null;
-		 
-		  try {
-		   ps = con.prepareStatement("INSERT INTO COURSE VALUES(?,?,?)");
-		   ps.setInt(1, 6);
-		   ps.setString(2, "Lewis");
-		   ps.setString(3, "JDBC");
-		   ps.executeUpdate(); // executes the insert query
-		    
-		  }
-		  catch (Exception e) {
-		   System.out.println("ERROR executing query: ");
-		   e.printStackTrace();
-		  }
-		  finally {
-		   try {
-		    //close the statement
-		    ps.close();
-		   } catch (SQLException e) {
-		    e.printStackTrace();
-		   }
-		  }
-		 }
-		  
-		  
-		 public void executeSelectQuery(Connection con) {
-		  PreparedStatement ps = null;
-		   
-		  try {
-		   ps = con.prepareStatement("SELECT COURSE_ID, NAME, COURSE  FROM COURSE");
-		   ResultSet rs = ps.executeQuery(); // read from database
-		   while(rs.next()){
-		    Integer id = rs.getInt("COURSE_ID");
-		    String name = rs.getString("NAME");
-		    String course = rs.getString("COURSE");
-		    System.out.println("id:" + id + ", name:" + name + ", course:" + course);
-		   }
-		    
-		  }
-		  catch (Exception e) {
-		   System.out.println("ERROR executing query: ");
-		   e.printStackTrace();
-		  }
-		  finally{
-		   try {
-		    ps.close();
-		   } catch (SQLException e) {
-		    e.printStackTrace();
-		   }
-		  }
-		 }
-		 
-		 
-		 //main method
-		 public static void main(String[] args) {
-		   
-		  JdbcTemplate tut = new JdbcTemplate();
-		   
-		  //1. get the connection to the database
-		  final Connection con = tut.getConnection();
-		   
-		  //2. Insert a record via JDBC
-		  tut.executeInsertQuery(con);
-		   
-		  //3. select all records from the database
-		  tut.executeSelectQuery(con);
-		   
-		   
-		  //4. close the connection to the databse
-		  try {
-		   con.close();
-		  } catch (SQLException e) {
-		   e.printStackTrace();
-		  }
-		 }
+	private JdbcTemplate() {
+	}
+
+	public synchronized static JdbcTemplate getMySQLJdbcTemplate() {
+		if (jdbcTemplate == null)
+			jdbcTemplate = new JdbcTemplate();
+		jdbcTemplate.initializeConnection();
+		return jdbcTemplate;
+	}
+
+	private void initializeConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(
+					"jdbc:mysql://localhost/public_health_portal", "root", "root");
+		} catch (SQLException e) {
+			System.out.println("ERROR: failed to connect to the databse");
+			e.printStackTrace();
+		} catch (ClassNotFoundException cnfe) {
+			System.out.println("ERROR: failed to load MySQL JDBC driver.");
+			cnfe.printStackTrace();
+		}
+	}
+
+	public int executeUpdate(String sqlQuery) {
+		PreparedStatement ps = null;
+		try {
+			ps = connection.prepareStatement(sqlQuery);
+			return ps.executeUpdate(); // executes the insert query
+		} catch (Exception e) {
+			System.out.println("ERROR executing query: ");
+			e.printStackTrace();
+		} finally {
+			try {
+				ps.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return -1;
+	}
+
+	public ResultSet executeQuery(String sqlQuery) {
+		PreparedStatement ps = null;
+		try {
+			ps = connection.prepareStatement(sqlQuery);
+			return ps.executeQuery(); // executes the insert query
+		} catch (Exception e) {
+			System.out.println("ERROR executing query: ");
+			e.printStackTrace();
+		} finally {
+			try {
+				ps.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
 }
