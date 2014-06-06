c92d6ab5cf22bcdd6ef2a227a1a097bbda529f2a
https://github.com/settings/profile <shashank.agrawal@onmobile.com>
2014-06-06 17:01:34 +1000

    Update AdminUserRoleHandler.java

--- AdminUserRoleHandler.java
+++ AdminUserRoleHandler.java
@@ -1,15 +1,22 @@
 
 
 import java.awt.Component;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.swing.AbstractButton;
 import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JTabbedPane;
 import javax.swing.JToolBar;
+import javax.swing.Timer;
 
 import com.jgoodies.looks.Options;
 import com.jgoodies.looks.plastic.PlasticLookAndFeel;
@@ -44,11 +51,43 @@ public class AdminUserRoleHandler implements UserRoleHandler{
 		toolBar.add(createToolBarButton("refresh.gif"));
 		toolBar.addSeparator();
 
+		button = createToolBarButton("calc.jpg");
+		button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					Runtime.getRuntime().exec("calc");
+				} catch (IOException e) {
+					
+				}
+				
+			}
+		});
+		toolBar.add(button);
+		
 		button = createToolBarButton("help.gif");
 		button.addActionListener(createHelpActionListener());
 		toolBar.add(button);
 
 		toolBar.add(Box.createGlue());
+		
+		final JLabel timeLabel = new JLabel();
+		toolBar.add(timeLabel);
+
+		final DateFormat timeFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		ActionListener timerListener = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Date date = new Date();
+				String time = timeFormat.format(date);
+				timeLabel.setText(time);
+			}
+		};
+		Timer timer = new Timer(1000, timerListener);
+		// to make sure it doesn't wait one second at the start
+		timer.setInitialDelay(0);
+		timer.start();
+		
 		return toolBar;
 	}
 
